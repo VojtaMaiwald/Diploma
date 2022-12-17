6bf1afb3b3175bec0913c5f53c285493387a79c8
Vojtěch Maiwald <vojtech.maiwald@storaenso.com>
2022-12-17 13:06:24 +0100

    validation

--- test.py
+++ test.py
@@ -1,46 +1,73 @@
 import tensorflow as tf
 import cv2 as cv
 import numpy as np
+import glob
+import os
+import re
 
 WEBCAM = False
-MODEL_PATH = ".\\_full_model"
+ONE_IMG_TEST = False
+MODEL_PATH = ".\\nets\\full\\_full_model"
 TEST_IMAGES_PATH = "C:\\Users\\Vojta\\DiplomaProjects\\AffectNet\\val_set\\images\\"
 TEST_LABELS_PATH = "C:\\Users\\Vojta\\DiplomaProjects\\AffectNet\\val_set\\all_labels_exp.npy"
 DICT = {0: "Neutral", 1: "Happiness", 2: "Sadness", 3: "Surprise", 4: "Fear", 5: "Disgust", 6: "Anger",
 7: "Contempt", 8: "None", 9: "Uncertain", 10: "No-Face"}
 
 def webcam(model):
-    cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
-    capture = cv.VideoCapture(0)
+	cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+	capture = cv.VideoCapture(0)
 
-    while (True):
-        ret, image = capture.read()
-        rectangles = cascade.detectMultiScale(image, 1.3, 3)
+	while (True):
+		ret, image = capture.read()
+		rectangles = cascade.detectMultiScale(image, 1.3, 3)
 
-        for rect in rectangles:
-            img = image[rect[1]:(rect[1] + rect[3]), rect[0]:(rect[0] + rect[2])]
-            img = cv.resize(img, (224, 224))
-            img = img.reshape(1, 224, 224, 3)
-            prediction = model.predict(img)[0]
-            print(prediction)
-            cv.rectangle(image, rect, (0, 0, 0), 3)
-            cv.rectangle(image, rect, (255, 255, 255), 1)
-            cv.putText(image, DICT[np.argmax(prediction)], (rect[1], rect[0]), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 3, 2)
-            cv.putText(image, DICT[np.argmax(prediction)], (rect[1], rect[0]), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, 2)
+		for rect in rectangles:
+			img = image[rect[1]:(rect[1] + rect[3]), rect[0]:(rect[0] + rect[2])]
+			img = cv.resize(img, (224, 224))
+			img = img.reshape(1, 224, 224, 3)
+			prediction = model.predict(img)[0]
+			print(prediction)
+			cv.rectangle(image, rect, (0, 0, 0), 3)
+			cv.rectangle(image, rect, (255, 255, 255), 1)
+			cv.putText(image, DICT[np.argmax(prediction)], (rect[1], rect[0]), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 3, 2)
+			cv.putText(image, DICT[np.argmax(prediction)], (rect[1], rect[0]), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, 2)
 
-        cv.imshow("webcam", image)
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
+		cv.imshow("webcam", image)
+		if cv.waitKey(1) & 0xFF == ord('q'):
+			break
 
-def test_imgs(model):
-    img = cv.imread("test_imgs\\angry.jpg", 1)
-    cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
-    rect = cascade.detectMultiScale(img, 1.3, 3)[0]
-    img = img[rect[1]:(rect[1] + rect[3]), rect[0]:(rect[0] + rect[2])]
-    img = cv.resize(img, (224, 224))
-    img = img.reshape(1, 224, 224, 3)
-    prediction = model.predict(img)[0]
-    print(DICT[np.argmax(prediction)], prediction[np.argmax(prediction)], prediction)
+def testOneImage(model):
+	img = cv.imread("test_imgs\\angry.jpg", 1)
+	cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+	rect = cascade.detectMultiScale(img, 1.3, 3)[0]
+	img = img[rect[1]:(rect[1] + rect[3]), rect[0]:(rect[0] + rect[2])]
+	img = cv.resize(img, (224, 224))
+	img = img.reshape(1, 224, 224, 3)
+	prediction = model.predict(img)[0]
+	print(DICT[np.argmax(prediction)], prediction[np.argmax(prediction)], prediction)
+
+def testValDataset(model):
+	labels = np.load(TEST_LABELS_PATH)
+	images_paths_list = glob.glob(TEST_IMAGES_PATH + "*.jpg")
+	images_paths_list.sort(key = natural_keys)
+	errors = 0
+
+	for i in len(images_paths_list):
+		img_path = images_paths_list[i]
+		img = cv.imread(img_path, 1)
+		img = img.reshape(1, 224, 224, 3)
+		prediction = model.predict(img)[0]
+		if int(prediction[np.argmax(prediction)]) != int(labels[i]):
+			errors += 1
+
+	evaluation = errors / len(images_paths_list)
+	print(f"Errors: {errors}\t evaluation: {evaluation} %")
+
+def atoi(text):
+	return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+	return [ atoi(c) for c in re.split(r'(\d+)', text) ]
 
 def init():
 	gpus = tf.config.list_physical_devices('GPU')
@@ -56,10 +83,14 @@ def init():
 			print(e)
 
 if __name__ == '__main__':
-    if WEBCAM:
-        model = tf.keras.models.load_model(MODEL_PATH)
-        webcam(model)
-    else:
-        init()
-        model = tf.keras.models.load_model(MODEL_PATH)
-        test_imgs(model)
+	if WEBCAM:
+		model = tf.keras.models.load_model(MODEL_PATH)
+		webcam(model)
+	elif ONE_IMG_TEST:
+		init()
+		model = tf.keras.models.load_model(MODEL_PATH)
+		testOneImage(model)
+	else:
+		init()
+		model = tf.keras.models.load_model(MODEL_PATH)
+		testValDataset(model)
