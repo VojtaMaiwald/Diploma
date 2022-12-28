3385666f7370f0129befad7e4cd91262022f6673
VojtaMaiwald <vo.maiwald@gmail.com>
2022-12-29 00:17:41 +0100

    tflite conversion and validation

---
+++ convert_to_tflite.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+MODEL_PATH = ".\\nets\\full\\_full_model"
+EXPORT_PATH = ".\\nets_tflite\\"
+
+# Convert the model
+converter = tf.lite.TFLiteConverter.from_saved_model(MODEL_PATH) # path to the SavedModel directory
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+#converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+print(converter.inference_input_type, converter.inference_output_type, converter.target_spec.supported_ops)
+#converter.inference_input_type = tf.int8  # or tf.uint8
+#converter.inference_output_type = tf.int8  # or tf.uint8
+tflite_model = converter.convert()
+
+# Save the model.
+#with open(EXPORT_PATH + "model_optimized_defaultType.tflite", 'wb') as f:
+#	f.write(tflite_model)
---
+++ test_tflite.py
@@ -0,0 +1,129 @@
+import numpy as np
+import tensorflow as tf
+import cv2 as cv
+import glob
+import re
+from keras.applications import MobileNetV2
+from keras.layers import Dense, Dropout, GlobalAveragePooling2D
+from keras.models import Model
+
+MODEL_PATH = ".\\nets_tflite\\model_experimental.tflite"
+#MODEL_PATH = ".\\nets_tflite\\model_optimized.tflite"
+DICT = {0: "Neutral", 1: "Happiness", 2: "Sadness", 3: "Surprise", 4: "Fear", 5: "Disgust", 6: "Anger", 7: "Contempt", 8: "None", 9: "Uncertain", 10: "No-Face"}
+TEST_IMAGES_PATH = "C:\\Users\\Vojta\\DiplomaProjects\\AffectNet\\val_set\\images\\"
+TEST_LABELS_PATH = "C:\\Users\\Vojta\\DiplomaProjects\\AffectNet\\val_set\\all_labels_exp.npy"
+
+TEST_ONE_IMG = False
+WEBCAM = False
+
+def testOneImage():
+	# Load TFLite model and allocate tensors.
+	interpreter = tf.lite.Interpreter(model_path = MODEL_PATH)
+	interpreter.allocate_tensors()
+
+	# Get input and output tensors.
+	input_details = interpreter.get_input_details()
+	output_details = interpreter.get_output_details()
+
+	# Test model on image
+	#img = cv.imread("test_imgs\\angry.jpg", 1)
+	#img = cv.imread("test_imgs\\happy.jpg", 1)
+	#img = cv.imread("test_imgs\\happy2.jpg", 1)
+	#img = cv.imread("test_imgs\\sad.jpg", 1)
+	img = cv.imread("test_imgs\\surprise.jpg", 1)
+	cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+	rect = cascade.detectMultiScale(img, 1.3, 3)[0]
+	img = img[rect[1]:(rect[1] + rect[3]), rect[0]:(rect[0] + rect[2])]
+	img = cv.resize(img, (224, 224))
+	img = img.reshape(1, 224, 224, 3)
+	img = np.array(img, dtype = np.float32)
+	#img = img / 255.0
+
+	interpreter.set_tensor(input_details[0]['index'], img)
+	interpreter.invoke()
+	output = interpreter.get_tensor(output_details[0]["index"])[0]
+	#print(output)
+	print(DICT[np.argmax(output)], output[np.argmax(output)], output)
+
+def webcamTest():
+	cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+	capture = cv.VideoCapture(0)
+
+	while (True):
+		ret, image = capture.read()
+		rectangles = cascade.detectMultiScale(image, 1.3, 3)
+		# Load TFLite model and allocate tensors.
+		interpreter = tf.lite.Interpreter(model_path = MODEL_PATH)
+		interpreter.allocate_tensors()
+
+		# Get input and output tensors.
+		input_details = interpreter.get_input_details()
+		output_details = interpreter.get_output_details()
+
+		for rect in rectangles:
+			img = image[rect[1]:(rect[1] + rect[3]), rect[0]:(rect[0] + rect[2])]
+			img = cv.resize(img, (224, 224))
+			img = img.reshape(1, 224, 224, 3)
+			img = np.array(img, dtype = np.float32)
+			interpreter.set_tensor(input_details[0]['index'], img)
+			interpreter.invoke()
+			output = interpreter.get_tensor(output_details[0]["index"])[0]
+			emotion = DICT[np.argmax(output)]
+			prediction = output[np.argmax(output)]
+			print(f"{emotion} {(prediction * 100):.3f} %         ", end = "\r")
+			cv.rectangle(image, rect, (0, 0, 0), 3)
+			cv.rectangle(image, rect, (255, 255, 255), 1)
+			cv.putText(image, emotion, (rect[1], rect[0]), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 3, 2)
+			cv.putText(image, emotion, (rect[1], rect[0]), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, 2)
+
+		cv.imshow("webcam", image)
+		if cv.waitKey(1) & 0xFF == ord('q'):
+			break
+
+def testValDataset():
+	labels = np.load(TEST_LABELS_PATH)
+	images_paths_list = glob.glob(TEST_IMAGES_PATH + "*.jpg")
+	images_paths_list.sort(key = natural_keys)
+	errors = 0
+	errorsUnderPercent = [0] * 10
+
+	# Load TFLite model and allocate tensors.
+	interpreter = tf.lite.Interpreter(model_path = MODEL_PATH)
+	interpreter.allocate_tensors()
+
+	# Get input and output tensors.
+	input_details = interpreter.get_input_details()
+	output_details = interpreter.get_output_details()
+
+	for i in range(len(images_paths_list)):
+		img_path = images_paths_list[i]
+		img = cv.imread(img_path, 1)
+		
+		img = img.reshape(1, 224, 224, 3)
+		img = np.array(img, dtype = np.float32)
+
+		interpreter.set_tensor(input_details[0]['index'], img)
+		interpreter.invoke()
+		prediction = interpreter.get_tensor(output_details[0]["index"])[0]
+		if np.argmax(prediction) != labels[i]:
+			errors += 1
+			errorsUnderPercent[int(prediction[np.argmax(prediction)] * 10)] += 1
+		evaluation = (1 - (errors / (i + 1))) * 100
+		print(f"{i} / {len(images_paths_list)}\t\tSuccess rate: {evaluation:.3f} %\t\tErrors by probability: {errorsUnderPercent}        ", end = "\r")
+
+	evaluation = (1 - (errors / (len(images_paths_list)))) * 100
+	print(f"Images: {len(images_paths_list)}\t\tErrors: {errors}\t\tSuccess rate: {evaluation:.3f} %\t\tErrors by probability: {errorsUnderPercent}            ")
+
+def atoi(text):
+	return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+	return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+
+if __name__ == '__main__':
+	if TEST_ONE_IMG:
+		testOneImage()
+	elif WEBCAM:
+		webcamTest()
+	else:
+		testValDataset()
